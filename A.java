a22e28fc8402cc97399e5f250f066c1304bae57b
Wellington Costa <wellingtoncostagrilo2018@gmail.com>
2026-04-16 12:22:13 -0300

    feat: adiciona classes A e B

---
+++ A.java
@@ -0,0 +1,14 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    public int MA1(){
+        System.out.println("Método: MA1()");
+        return A1;
+    }
+
+    public float MA2(){
+        System.out.println("Método: MA2()");
+        return A2;
+    }
+}
---
+++ B.java
@@ -0,0 +1,14 @@
+public class B {
+    private int B1;
+    private int B2;
+
+    public int MB1(){
+        System.out.println("Método: MB1()");
+        return B1;
+    }
+
+    public float MB2(){
+        System.out.println("Método: MB2()");
+        return B2;
+    }
+}
