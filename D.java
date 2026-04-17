bf0dd382b9d282ef657de2317799cb6e48978678
Wellington Costa <wellingtoncostagrilo2018@gmail.com>
2026-04-17 10:07:54 -0300

    Cria classe D com métodos MD1 e MD2

---
+++ D.java
@@ -0,0 +1,26 @@
+public class D {
+    private int D1;
+    private String D2;
+
+    public int getD1() {
+        return D1;
+    }
+    public String getD2() {
+        return D2;
+    }
+    public void setD1(int D1) {
+        this.D1 = D1;
+    }
+    public void setD2(String D2) {
+        this.D2 = D2;
+    }
+    public void MD1() {
+        System.out.println("Método: MD1()");
+    }
+    public void MD2() {
+        System.out.println("Método: MD2()");
+    }
+    public void MD3() {
+        System.out.println("Método: MD3()");
+    }
+}
