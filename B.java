1067cd81ea8284c543a66703bdeb6a2d9ff91f8d
Wellington Costa <wellingtoncostagrilo2018@gmail.com>
2026-04-17 10:05:01 -0300

    Adiciona o método MB3 na classe B e adiciona get e set de questões anteriores

--- B.java
+++ B.java
@@ -1,14 +1,30 @@
 public class B {
     private int B1;
-    private int B2;
+    private float B2;
 
-    public int MB1(){
-        System.out.println("Método: MB1()");
+    public int getB1(){
         return B1;
     }
-
-    public float MB2(){
-        System.out.println("Método: MB2()");
+    public float getB2(){
         return B2;
     }
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2){
+        this.B2 = B2;
+    }  
+
+    public void MB1(){
+        System.out.println("Método: MB1()");
+    }
+
+    public void MB2(){
+        System.out.println("Método: MB2()");
+    }
+    public void MB3(){
+        System.out.println("Método: MB3()");
+    }
+    
 }
