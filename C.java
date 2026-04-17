c7c9141ea668d59e77172f58004bcf66347ffd6d
Wellington Costa <wellingtoncostagrilo2018@gmail.com>
2026-04-17 10:05:53 -0300

    Adiciona método MC3 na classe C

--- C.java
+++ C.java
@@ -17,9 +17,13 @@ public class C {
         this.C2=C2;
     }
     public void MC1(){
-        System.out.println("MC1");
+        System.out.println("Método: MC1");
     }
     public void MC2(){
-        System.out.println("MC2");
+        System.out.println("Método: MC2");
+    }
+
+    public void MC3(){
+        System.out.println("Método: MC3");
     }
 }
